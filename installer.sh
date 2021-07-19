cfec9d4fe469289a7165860c5920d8e633bb819a
minhtricute69 <minhtricute69@gmail.com>
2021-07-19 17:10:55 +0700

    Cập nhật wifi + Ethernet

--- installer.sh
+++ installer.sh
@@ -1,80 +1,69 @@
 #!/bin/bash 
 install(){
-	echo "Do you want to connect to a network [y/n]?"
-	read a
-	if [$a=="y" || $a="N"] 
-then
+	echo "Connecting to a network"
 		echo "Select connection type:"
-		net=("Wifi","Ethernet")
+		net=("Wifi" "Ethernet")
 	select nt in ${net[*]}; do
 		case $nt in
-			"Wifi") wifi
+			"Wifi") wifidct
 					break;;
 			"Ethernet") ethernet
 					break;;
-			*) echo "Invalid Option! Try Again"
+			 *) echo "Invalid Option! Try Again"
 						;;
 		esac
 	done
-else [$a=="n" || $a=="N"]
-			echo "We'll move to partion disk![y/n]"
-			read b
-		if [$b=="y"]
-then
-			partition
-else [$b=="n"]
-			exit
-	fi
 }
-update(){}
-upgrade(){}
-quit(){}
 wifidct(){
 	echo "Detecting your wifi network interface . . ."
-	wlan=( /sys/class/net | grep wl*))
+	wlan=($(ls /sys/class/net | grep wl*))
 	i=1
 	for j in ${wlan[@]};do
 		echo "${i}) ${j}"
 		i=$((i+1))
 	done
-	echo "Enter your wlan network interface:"
-		read $c
+	read -r  -p  'Enter your wlan network interface:' c
 	ifconfig $c up 
-	if [$? != 0] 
-then
-	echo "Try again! May be your network interface doesn't exist!!!"
-	echo "Enter your wifi interface again:"
-	read $c
-	ifconfig $c up
-	wifichk
-else [$?==0]
-		echo "enter your Wifi SSID:"
-		read ssid
-		wifitype=("WPA","WPA-PSK","WEP","OPEN WIFI", "HIDDEN WIFI")
+	if [ $?=0 ] 
+	then
+		read -rp  'enter your Wifi SSID: ' ssid
+		wifitype=("WPA" "WPA-PSK" "WEP" "OPEN WIFI" "HIDDEN WIFI")
 		select wft in ${wifitype[@]};do
 			case $wft in
-			"WPA"|"WPA_PSK"|"WEP") echo "Enter wifi password:"
-									read password
-									nmcli device wifi connect $ssid password $password ifname $c
+			"WPA"|"WPA-PSK"|"WEP") read -r -p 'Enter wifi password: ' password
+									nmcli device wifi connect "${ssid}" password $password ifname $c
 									break;;
 			"OPEN WIFI")echo "Getting connect to your wifi..."
-						nmcli device wifi connect $ssid  ifname $c
+						nmcli device wifi connect "${ssid}"  ifname $c
 						break;;
-			"HIDDEN WIFI") echo "Enter wifi password
+			"HIDDEN WIFI") echo "Enter wifi password"
+			read password
+			nmcli device wifi connect "${ssid}" password $password ifname $c hidden yes;;
 			*) echo "Invalid Options"
 				;;
 			esac
 		done
+
+elif [ $?!=0 ]
+then
+
+	echo "Try again! May be your network interface doesn't exist!!!"
+	echo -n "Enter your wifi interface again: "
+	read c
+	ifconfig $c up
+	wifichk
+fi
 	}
 wifichk(){
-	if [$?!=0] 
+	if [ $?!=0 ] 
 	then 
 	echo "Try to config wifi again"
 	wifidct
+fi
 	}
 ethernet(){
 	echo "Detecting your Ethernet interface"
-	ethernetinf=$((ls /sys/class/net/ | grep ^e))
+	ethernetinf=($(ls /sys/class/net/ | grep ^e))
 	i=1
 	for j in ${ethernetinf[@]}; do
 		echo "${i}) ${j}"
@@ -82,7 +71,7 @@ ethernet(){
 	done
 	echo "Enter your Ethernet network interface:"
 	read $ethernet
-
+}
 echo  "Welcome to HydroOS Installer v1.0"
 echo "--------------HydroOS------------------"
 echo "What do you want me to do?"
@@ -103,3 +92,4 @@ case $opt in
 *) echo "Invalid Option! Please try again!!!";;
 esac
 done
+
